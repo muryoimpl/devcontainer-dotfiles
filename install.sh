a1343fd2b85808031e19c49dd3ae7d78f3a76c87
muryoimpl <muryoimpl@gmail.com>
2024-07-07 00:01:05 +0900

    Add tig

--- install.sh
+++ install.sh
@@ -3,9 +3,11 @@
 echo "source ~/devcontainer-dotfiles/index.zsh" >> ~/.zshrc
 
 sudo apt-get update && DEBIAN_FRONTEND=noninteractive sudo apt-get install -y xclip tmux neovim \
-    silversearcher-ag bat ripgrep nodejs gh
+    silversearcher-ag bat ripgrep nodejs gh tig
 
 mkdir -p ~/.local/bin && ln -s /usr/bin/batcat ~/.local/bin/bat
 
 cp -f ~/devcontainer-dotfiles/.gitconfig ~/.gitconfig
 cp -f ~/devcontainer-dotfiles/.irbrc ~/.irbrc
+mkdir -p ~/.config/tig
+cp -f ~/devcontainer-dotfiles/config/tig/config ~/.config/tig/
